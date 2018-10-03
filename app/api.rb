580e0713de20e630f83ebf7f3d313228244be337
eric-klimowich <eklimowich@hotmail.com>
2018-10-03 15:08:56 -0400

    Attempted downloads from api

--- app/api.rb
+++ app/api.rb
@@ -23,6 +23,70 @@ def api(character)
 
 end
 
+  def get_characters
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    characters = response_hash["results"].map do |character|
+      character["name"]
+    end
+  end
+
+  def get_character_heights
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    heights = response_hash["results"].map do |character|
+      character["height"]
+    end
+  end
+
+  def get_planets
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    planets = response_hash["results"].map do |planet|
+      planet["name"]
+    end
+  end
+
+  def get_starships
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    starships = response_hash["results"].map do |starship|
+      starship["name"]
+    end
+  end
+
+  def get_starship_speeds
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    starship_speeds = response_hash["results"].map do |starship|
+      starship["max_atmosphering_speed"]
+    end
+  end
+
+  def get_films
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    films = response_hash["results"].map do |film|
+      film["title"]
+    end
+  end
+
+  def get_director
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    films = response_hash["results"].map do |film|
+      film["director"]
+    end
+  end
+
+  def get_release_date
+    response_string = RestClient.get('http://www.swapi.co/api/people/')
+    response_hash = JSON.parse(response_string)
+    films = response_hash["results"].map do |film|
+      film["release_date"]
+    end
+  end
+
 def print_movies(films_hash)
 
   if films_hash != nil
